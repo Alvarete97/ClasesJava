583dd9dd9423828bbe4d2f9dc60f6458064c5e7b
Álvaro <74109026+Alvarete97@users.noreply.github.com>
2023-03-28 16:55:12 +0200

    Add files via upload

---
+++ Modelo_Examen_3/src/MAIN.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class MAIN {
+	
+	
+	public static void main(String[] args) throws IOException 
+	{
+		
+	}
+}
---
+++ Modelo_Examen_3/src/Pelicula.java
@@ -0,0 +1,15 @@
+
+public class Pelicula {
+	
+	String nombre;
+	int estreno, recaudacion;
+	
+	public void setNombre(String a) {nombre =a;};
+	public void setFecha(int a) {estreno = a;};
+	public void setRecaudacion(int a) {recaudacion = a;};
+	
+	public String getNombre() {return nombre;};
+	public int getEstreno() {return estreno;};
+	public int getRecaudacion() {return recaudacion;};
+	
+}
